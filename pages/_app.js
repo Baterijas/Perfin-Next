fb99025fb20f331201e3b87249423e453a1e247b
Raiens <101168589+Baterijas@users.noreply.github.com>
2024-10-25 17:03:38 +0000

    commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,18 @@
 import React from "react";
 import "./style.css";
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/react';
+import { useRouter } from 'next/router';
 
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();  // Get the current route
+
   return (
     <>
       <Component {...pageProps} />
       <Analytics />
+      <SpeedInsights route={router.pathname} />  {/* Add Speed Insights with route */}
     </>
   );
 }
